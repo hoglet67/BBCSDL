e39afa058a5b1a304c47ed41967d83900bd96e20
David Banks <dave@hoglet.com>
2023-09-11 12:11:10 +0100

    RISC-V Co Processor: Update error handler for 32-bit error code

--- console/riscv/crt0.s
+++ console/riscv/crt0.s
@@ -7,8 +7,8 @@ _start:
 
 # Called with a0 pointing to the error code
 error_handler:
-   addi    a1, a0, 1          # a1 = error smessage
-   lbu     a0, (a0)           # a0 = error code
+   addi    a1, a0, 4          # a1 = error smessage
+   lw      a0, (a0)           # a0 = error code
 # pass on to BBC Basic's error handler:
 #     void error(int, const char *);
 # this never returns
